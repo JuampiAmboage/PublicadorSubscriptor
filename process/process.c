523001d68cd6e314cbe30cdabe62d043ca2854e7
joaco <jordonez@fi.uba.ar>
2023-06-05 17:13:00 -0300

    creo carpetas para dividir el proyecto, creo el archivo process.c para manejar los procesos

---
+++ process/process.c
@@ -0,0 +1,16 @@
+
+// inicia un proceso
+void initialize() {
+
+}
+
+// Termina un proceso
+void kill() {
+
+}
+
+// Recibe un proceso y le hace un hijo
+// aunque no se si tendria que ir en la clase por temas de incompatiblidad entre procesos
+void newThread() {
+
+}
